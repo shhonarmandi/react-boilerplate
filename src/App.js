2802459dcf564491347720b5feeb7c82aabea557
Mohsen Honarmandi <shhonarmandi@outlook.com>
2022-05-20 21:09:27 +0430

    Develop (#4)
    * add packege.json
    
    * add manifest.json, favicon, and logo
    
    * install react and react-dom
    
    * install react, webpack, and babel
    
    * Feature/initial config (#3)
    
    * improve build performance
    
    * add css-loader and assets-loader
    
    * add prettier
    
    * add pre-commit hook

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import logo from './assets/images/logo.svg'
+import styles from './_app.module.scss'
+
+function App() {
+	return (
+		<div className={styles.container}>
+			<header className={styles.container__header}>
+				<img src={logo} className={styles.container__logo} alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a className={styles.container__link} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+					Learn React
+				</a>
+			</header>
+		</div>
+	)
+}
+
+export default App
